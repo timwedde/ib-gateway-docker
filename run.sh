e733d69f0dba5c1eb46b593ea2913378d6e7630c
Tim Wedde <timwedde@icloud.com>
2020-01-16 17:14:56 +0100

    Revert "Drop ENV variables as the credentials can be grabbed from the config"
    This reverts commit f897855d2d75b619871bd0343690461acf965a54.

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ socat TCP-LISTEN:$TWS_PORT,fork TCP:localhost:4001,forever &
 
 # Start this last and directly, so that if the gateway terminates for any reason, the container will stop as well.
 # Retry behavior can be implemented by re-running the container.
-/opt/ibc/scripts/ibcstart.sh $(ls ~/Jts/ibgateway) --gateway "--mode=$TRADING_MODE"
+/opt/ibc/scripts/ibcstart.sh $(ls ~/Jts/ibgateway) --gateway "--mode=$TRADING_MODE" "--user=$TWSUSERID" "--pw=$TWSPASSWORD"
